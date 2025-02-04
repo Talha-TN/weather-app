ba367887f5528034491fd12d10c710aa71c4b2bc
Talha-TN <nawazt964@gmail.com>
2025-02-04 15:56:47 -0800

    updated

--- src/assets/components/ShowWeather.jsx
+++ src/assets/components/ShowWeather.jsx
@@ -13,7 +13,6 @@ const ShowWeather = ({setCity,handleButton,error,weatherData}) => {
             onChange={(e) => setCity(e.target.value)} 
             className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
-
         <button 
             onClick={handleButton}
             className="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 rounded-lg transition duration-300"
